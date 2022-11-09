c74ce8c585b3cb26a4b509cbe0a3010a2a2fc99c
tancoder24 <tanmaygupta2408@gmail.com>
2022-11-09 21:18:08 +0530

    requirements added

--- commerce/settings.py
+++ commerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "auctions")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
